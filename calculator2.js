653402d02c6a982ca2e3b29678b3e103668ce527
Seanjkennedy <seanjkennedy@hotmail.co.uk>
2021-08-10 12:06:27 +0100

    updated comments

--- calculator2.js
+++ calculator2.js
@@ -1,18 +1,20 @@
 var clearDisplay = () => {
 
-    document.getElementById("display").innerHTML = "0";
+    document.getElementById("display").innerHTML = "0"; //resets display to 0
 }
 
 var equals = () => {
 
     var getDisplayData = document.getElementById("display").innerHTML;
-    var setCalculationResult = eval(getDisplayData);
-    document.getElementById("display").innerHTML = setCalculationResult;
+    var setCalculationResult = eval(getDisplayData);  // the equation in the display is calculated
+    document.getElementById("display").innerHTML = setCalculationResult;  //result sent back to display
 }
 
 var decimal = () => {
 
     var lastInput = document.getElementById("display").innerHTML;
+        
+    // prevent adding decimals following a symbol 
     last = lastInput[lastInput.length - 1];
     if (last !== "+" && last !== "/" && last !== "*" && last !== "-" && last !== ".") {
         display.innerHTML += ".";
@@ -22,6 +24,8 @@ var decimal = () => {
 var getNumber = (id) => {
 
     var temp = document.getElementById(id).value;
+
+    // prevents leading zeros
     if (display.innerHTML === "0") {
         display.innerHTML = temp;
     } 
@@ -29,6 +33,7 @@ var getNumber = (id) => {
         display.innerHTML += temp;
     }
 
+    // re-enables the symbol buttons
     var elems = document.getElementsByClassName("symbol");
     for (var i = 0; i < elems.length; i++) {
         elems[i].disabled = false;
@@ -40,15 +45,10 @@ var getSymbol = (id) => {
     var temp = document.getElementById(id);
     display.innerHTML += temp.value;
 
+    // disables the symbol buttons
     var elems = document.getElementsByClassName("symbol");
     for (var i = 0; i < elems.length; i++) {
         elems[i].disabled = true;
     }
-
-}
-
-var disableSymbols = () => {
-
-    document.getElementsByClassName("subtract button").disabled = true;
 
 }
